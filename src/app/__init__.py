04058ba29ff60744c994b7fe3e3d05283c7ebc6f
nikhil <nsingh@cdac.in>
2025-05-23 11:29:01 +0530

    on process

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,6 +1,9 @@
+import json
 from flask import Flask, request, jsonify
 import sys
 import os
+
+from kafka import KafkaProducer
 
 # Add src to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
@@ -9,16 +12,23 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 messageService = MessageService()
-
-@app.route('/v1/ds/message/', methods=['POST'])
+producer = KafkaProducer(bootstrap_servers=['3.111.213.48:9092'],
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+@app.route('/v1/ds/message', methods=['POST'])
 def handle_message():
     message = request.json.get('message')
+
     result = messageService.process_message(message)
-    return jsonify(result.dict())
+    # Convert to dict for both jsonify and Kafka
+    result_dict = result.dict()
+    # Send to Kafka
+    producer.send('expense_service', result_dict)
+
+    return jsonify(result_dict)
 
 @app.route('/', methods=['GET'])
 def handle_get():
     return "Hello World"
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=9000, debug=True)
+    app.run(host="localhost", port=7000, debug=True)
